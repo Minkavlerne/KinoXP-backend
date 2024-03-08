1f667e243d9ad17ca2c13d96e94f54f0bf8f0043
sebbex1337 <sefort@hotmail.dk>
2024-03-08 12:38:37 +0100

    init maven

---
+++ src/test/java/org/example/kinoxpbackend/KinoXpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kinoxpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
