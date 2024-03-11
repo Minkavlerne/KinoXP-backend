e9311ba1b147adab622fc96785e1eaf9dc658f9f
Jasa95 <jaco3020@stud.kea.dk>
2024-03-11 14:23:32 +0100

    MIN-7 Movie entity created

---
+++ src/main/java/org/example/kinoxpbackend/Movie.java
@@ -0,0 +1,53 @@
+package org.example.kinoxpbackend;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Movie {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int Id;
+
+   @Column(nullable = false, unique = true)
+    private String title;
+   @Column(nullable = false)
+    private String description;
+
+   @Column(nullable = false)
+    private String posterBase64;
+   @Column(nullable = false)
+    private String posterUrl;
+   @Column(nullable = false)
+    private String trailerUrl;
+
+   @Column(nullable = false)
+    private int ageLimit;
+
+   @Column(nullable = false)
+    private LocalTime duration;
+
+
+   @CreationTimestamp
+    private LocalDateTime created_at;
+
+   @UpdateTimestamp
+    private LocalDateTime updated_at;
+
+
+}
