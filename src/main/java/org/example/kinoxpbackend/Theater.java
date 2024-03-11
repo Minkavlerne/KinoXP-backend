3a5ea82a0087352aca17ed53181f0357bcd2fd5d
Slumhunden <kroghbastin@gmail.com>
2024-03-11 14:23:27 +0100

    MIN-14 Theater entity created

---
+++ src/main/java/org/example/kinoxpbackend/Theater.java
@@ -0,0 +1,27 @@
+package org.example.kinoxpbackend;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Theater {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+    @UpdateTimestamp
+    private LocalDateTime edited_at;
+}
