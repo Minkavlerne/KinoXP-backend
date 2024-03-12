6d2e04cdac07061cb1ceffcb7149c450ab664aa2
Jasa95 <jaco3020@stud.kea.dk>
2024-03-12 09:13:41 +0100

    MIN-11-Entity-Seat
    Basic seat entity created

---
+++ src/main/java/Seat.java
@@ -0,0 +1,39 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+public class Seat {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int Id;
+
+    @Column(nullable = false)
+    private String rowNumber;
+    @Column(nullable = false)
+    private String seatNumber;
+
+    private boolean isReserved;
+
+    @Column(nullable = false)
+    private String type;
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+}
