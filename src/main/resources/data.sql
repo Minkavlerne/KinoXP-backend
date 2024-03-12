a4be3acd2123b59a176ae3b55b0286068c849976
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-03-12 10:28:53 +0100

    MIN-52
    MIN-52 data.sql dump

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Movies (Title, Description, PosterBase64, PosterURL, TrailerURL, AgeLimit, Duration)
+VALUES
+    ('The Shawshank Redemption', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 'base64data1', 'https://example.com/poster1.jpg', 'https://youtube.com/trailer1', 18, '02:22:00'),
+    ('The Godfather', 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', 'base64data2', 'https://example.com/poster2.jpg', 'https://youtube.com/trailer2', 18, '02:55:00'),
+    ('Pulp Fiction', 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.', 'base64data3', 'https://example.com/poster3.jpg', 'https://youtube.com/trailer3', 17, '02:34:00'),
+    ('The Dark Knight', 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.', 'base64data4', 'https://example.com/poster4.jpg', 'https://youtube.com/trailer4', 13, '02:32:00'),
+    ('Inception', 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', 'base64data5', 'https://example.com/poster5.jpg', 'https://youtube.com/trailer5', 13, '02:28:00');
