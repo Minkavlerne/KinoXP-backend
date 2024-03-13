d3b7e3c6cfb9c532904e1ba914d20672352bdd4e
Sofie Larsen <Sofieamalierl@hotmail.com>
2024-03-13 10:18:19 +0100

    MIN-66
    MIN-66 theater Delete created

--- src/main/java/org/example/kinoxpbackend/kino/services/TheaterService.java
+++ src/main/java/org/example/kinoxpbackend/kino/services/TheaterService.java
@@ -2,6 +2,7 @@ package org.example.kinoxpbackend.kino.services;
 
 import org.example.kinoxpbackend.kino.entity.Theater;
 import org.example.kinoxpbackend.kino.repository.TheaterRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +16,11 @@ public class TheaterService {
     }
         public List<Theater> getAllTheaters() {
             return theaterRepository.findAll();
+    }
+    public ResponseEntity deleteTheater(int id){
+        Theater theater = theaterRepository.findById(id).orElseThrow(()-> new RuntimeException("Theater not found"));
+        theaterRepository.delete(theater);
+        return new ResponseEntity(("Theater deleted"), org.springframework.http.HttpStatus.OK);
     }
 }
 
