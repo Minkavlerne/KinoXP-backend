183fe932ab8c4675aeb4f8c74af4d22e951458a8
Jasa95 <jaco3020@stud.kea.dk>
2024-03-18 13:55:02 +0100

    MIN-82
    In progress

--- src/main/java/org/example/kinoxpbackend/kino/services/BookingService.java
+++ src/main/java/org/example/kinoxpbackend/kino/services/BookingService.java
@@ -2,11 +2,15 @@ package org.example.kinoxpbackend.kino.services;
 
 import org.example.kinoxpbackend.kino.dto.BookingDto;
 import org.example.kinoxpbackend.kino.entity.Booking;
+import org.example.kinoxpbackend.kino.entity.Seat;
 import org.example.kinoxpbackend.kino.repository.BookingRepository;
 import org.example.kinoxpbackend.kino.repository.MovieShowRepository;
+import org.example.kinoxpbackend.kino.repository.SeatRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookingService {
@@ -14,6 +18,8 @@ public class BookingService {
     BookingRepository bookingRepository;
 
     MovieShowRepository movieShowRepository;
+
+    SeatRepository seatRepository;
 
 
     public BookingService(BookingRepository bookingRepository, MovieShowRepository movieShowRepository) {
@@ -28,6 +34,13 @@ public class BookingService {
 
     public BookingDto getBookingById(int id) {
         Booking booking = bookingRepository.findById(id).orElseThrow(() -> new RuntimeException("Booking not found"));
+        return new BookingDto(booking);
+    }
+
+    public BookingDto addBooking(BookingDto bookingDto) {
+        Booking booking = new Booking();
+        booking.setBookingNumber(bookingDto.getBookingNumber());
+        bookingRepository.save(booking);
         return new BookingDto(booking);
     }
 
