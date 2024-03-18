2dd5a7f22610dbf2f75888bd1d8bca053b6f6a10
Slumhunden <kroghbastin@gmail.com>
2024-03-18 09:18:27 +0100

    MIN-76 get seats
    Get Seats og Get Seats by ID

--- src/main/java/org/example/kinoxpbackend/kino/api/SeatController.java
+++ src/main/java/org/example/kinoxpbackend/kino/api/SeatController.java
@@ -3,7 +3,10 @@ package org.example.kinoxpbackend.kino.api;
 
 import org.example.kinoxpbackend.kino.dto.SeatDto;
 import org.example.kinoxpbackend.kino.entity.Seat;
+import org.example.kinoxpbackend.kino.repository.SeatRepository;
+import org.example.kinoxpbackend.kino.services.CategoryService;
 import org.example.kinoxpbackend.kino.services.SeatService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//@RequestMapping("/seats")
-//@RestController
+@RequestMapping("/seats")
+@RestController
 public class SeatController {
+    private final SeatService seatService;
+
+    public SeatController(SeatService seatService){
+        this.seatService = seatService;
+    }
+    @GetMapping
+    public ResponseEntity<List> getAllSeats(){
+        return ResponseEntity.ok(seatService.getAllSeats());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<SeatDto> getSeatById(@PathVariable int id){
+        return ResponseEntity.ok(seatService.getSeatById(id));
+    }
+
+
 }
