62fbe68804a5b07951a5c95db9ece0fa6eca982d
Jasa95 <jaco3020@stud.kea.dk>
2024-03-18 09:17:45 +0100

    Update BookingService.java

--- src/main/java/org/example/kinoxpbackend/kino/services/BookingService.java
+++ src/main/java/org/example/kinoxpbackend/kino/services/BookingService.java
@@ -2,9 +2,12 @@ package org.example.kinoxpbackend.kino.services;
 
 import org.example.kinoxpbackend.kino.dto.BookingDto;
 import org.example.kinoxpbackend.kino.entity.Booking;
+import org.example.kinoxpbackend.kino.entity.Movie;
 import org.example.kinoxpbackend.kino.entity.MovieShow;
 import org.example.kinoxpbackend.kino.repository.BookingRepository;
+import org.example.kinoxpbackend.kino.repository.MovieRepository;
 import org.example.kinoxpbackend.kino.repository.MovieShowRepository;
+import org.example.kinoxpbackend.kino.repository.SeatRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,4 +16,14 @@ import java.util.List;
 
 //@Service
 public class BookingService {
+
+    BookingRepository bookingRepository;
+
+    MovieShowRepository movieShowRepository;
+
+    SeatRepository seatRepository;
+
+    MovieRepository movieRepository;
+    
+
 }
